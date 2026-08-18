2743a272dc5800a5dccc9a16ab22745455a6d4d4
Trương Quốc Việt <65813680+VietTruong-50@users.noreply.github.com>
2026-08-18 15:01:47 +0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char c = s.charAt(right);
+
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(c);
+            ans = Math.max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+}
